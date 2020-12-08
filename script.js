527c3a52a5d27580bca1dbc7470a04157d9126be
Helen_Kerenykevych <helenchekanovska@gmail.com>
2020-12-08 09:57:55 +0200

    refactor: script.js

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-
-	const input = document.querySelector(".todo-input"),
-  ul = document.querySelector("ul.todo-list"),
-  btn = document.querySelector(".todo-btn"),
-  form = document.querySelector(".todo");
+const input = document.querySelector(".todo-input");
+const ul = document.querySelector("ul.todo-list");
+const form = document.querySelector(".todo");
 
 function createTodo() {
-const li = document.createElement("li"),
-    newTodo = input.value;
+  ul.insertAdjacentHTML("afterbegin", 
+  `
+    <li data-list-item class="todo-list-item"> 
+      <span class="todo-list-item-text">${input.value}</span> 
 
-li.innerHTML =` <li class="todo-list-item"> 
-
-  <span class="todo-list-item-text">${newTodo}</span> 
-
-  <button class="todo-list-item-trash">
-  <i class="fas fa-trash-alt"></i>
-  </button> 
-
-</li>`;
-
-input.value = "";
-listenDeleteTodo(button);
-
+      <button data-button class="todo-list-item-trash">
+        <i class="fas fa-trash-alt"></i>
+      </button> 
+    </li>
+  ` 
+  );
 }
 
-function listenDeleteTodo(element) {
-element.addEventListener("click", (event) => {
-element.parentElement.remove();
-event.stopPropagation();
-});
-}
+(function listenDeleteTodo() {
+  document.addEventListener("click", (event) => {  
+    const btn = event.target.closest("[data-button]");
+
+    if (btn) {
+      btn.closest("[data-list-item]").remove();
+    }
+  });
+})();
+
 
 form.addEventListener("submit", (event) => {
-event.preventDefault();
-if (input.value) {
-createTodo();
-}
+  event.preventDefault();
+
+  if (input.value) {
+    createTodo();
+    input.value = "";
+  }
 });
 
 
