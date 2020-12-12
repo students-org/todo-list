455a332dc1f6e3ab4141008f15f64b0c3bc36e5f
Helen_Kerenykevych <helenchekanovska@gmail.com>
2020-12-12 15:20:46 +0200

    refactor: add branch with implementation on classes

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const TODOs = document.querySelectorAll("[data-todo]");
+const tasksListEl = document.querySelector("[data-todo]");
 
- function createToDo ( {ul, form } ) {
-  function addTask(name) {
-    ul.insertAdjacentHTML("afterbegin", 
-    `
-      <li data-list-item class="todo-list__item"> 
-        <span class="todo-list__text">${name}</span> 
-  
-        <button data-button class="btn">
-          <i class="fas fa-trash-alt"></i>
-        </button> 
-      </li>
-    ` 
-    );
+const tasks = [
+  {
+    name: "Learn Java Script"
   }
+];
 
-  (function listenDeleteTodo() {
+class TasksList {
+  constructor ({ ul, form, tasks }){
+    this.ul = ul;
+    this.form = form;
+    this.tasks = tasks;
+  
+    this.init();
+  }
+  
+  init (){
     document.addEventListener("click", (event) => {  
       const btn = event.target.closest("[data-button]");
   
@@ -23,25 +23,57 @@ const TODOs = document.querySelectorAll("[data-todo]");
         btn.closest("[data-list-item]").remove();
       }
     });
-  })();
-  
-  form.addEventListener("submit", (event) => {
-    const input = event.target.querySelector("[name=name]");
-
-    event.preventDefault();
     
-    if (input.value) {
-      addTask();
-      input.value = "";
-    }
-  });
+    this.form.addEventListener("submit", (event) => {
+      const input = event.target.querySelector("[name=name]");
+  
+      event.preventDefault();
+      
+      if (input.value) {
+        this.addTask({ name: input.value });
+  
+        input.value = "";
+      }
+    });
+  
+    this.tasks.forEach(this.addTask);
+  }
+
+  addTask ({ name }){
+    const task = new Task({ name });
+  
+    this.ul.insertAdjacentHTML("afterbegin", task.getHTML());
+  }
 }
 
-TODOs.forEach((todo) => {
-  createToDo({ 
-    ul: todo.querySelector("[data-list]"),
-    form: todo.querySelector("[data-form]")
-  });
+class Task {
+  constructor({ name = '' }){
+    this.name = name;
+  }
+  
+  getHTML(){
+    return `
+      <li data-list-item class="todo-list__item"> 
+        <span class="todo-list__text">${this.name}</span> 
+        <button data-button class="btn">
+          <i class="fas fa-trash-alt"></i>
+        </button> 
+      </li>
+    `;
+  }
+}
+
+
+
+// TODOs.forEach((tasksList) => {
+//   new TasksList({ 
+//     ul: tasksList.querySelector("[data-list]"),
+//     form: tasksList.querySelector("[data-form]")
+//   });
+// });
+
+const tasksList = new TasksList({ 
+  ul: tasksListEl.querySelector("[data-list]"),
+  form: tasksListEl.querySelector("[data-form]"),
+  tasks 
 });
-
-
